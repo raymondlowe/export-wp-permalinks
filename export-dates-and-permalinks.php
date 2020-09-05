13c6ab5c59978516dec4b18eae8aa6b55cb48e8c
raymondlowe <49975086+raymondlowe@users.noreply.github.com>
2020-09-05 17:37:05 +0800

    source

---
+++ export-dates-and-permalinks.php
@@ -0,0 +1,40 @@
+<?php
+
+/* from https://stackoverflow.com/questions/3464701/export-list-of-pretty-permalinks-and-post-title */
+
+include "wp-load.php";
+
+$posts = new WP_Query('post_type=any&posts_per_page=-1&post_status=publish');
+$posts = $posts->posts;
+/*
+global $wpdb;
+$posts = $wpdb->get_results("
+    SELECT ID,post_type,post_title
+    FROM {$wpdb->posts}
+    WHERE post_status<>'auto-draft' AND post_type NOT IN ('revision','nav_menu_item')
+");
+*/
+
+header('Content-type:text/plain');
+foreach($posts as $post) {
+    $dateofpost = get_the_date('Y-m-d', $post->ID);
+    switch ($post->post_type) {
+        case 'revision':
+        case 'nav_menu_item':
+            break;
+        case 'page':
+            $permalink = get_page_link($post->ID);
+            break;
+        case 'post':
+            $permalink = get_permalink($post->ID);
+            break;
+        case 'attachment':
+            $permalink = get_attachment_link($post->ID);
+            break;
+        default:
+            $permalink = get_post_permalink($post->ID);
+            break;
+    }
+    /*echo "\n{$post->post_type}\t{$permalink}\t{$post->post_title}";*/
+    echo "{$dateofpost},{$permalink}\n";
+}
